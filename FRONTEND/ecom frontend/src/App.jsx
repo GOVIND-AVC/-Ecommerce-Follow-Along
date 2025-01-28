2961495e417f8ec2b4c6affb693bd76b73b8fd6c
GOVIND AVC <eemail2govindavc@gmail.com>
2025-01-28 12:07:41 +0530

    added signup

--- FRONTEND/ecom frontend/src/App.jsx
+++ FRONTEND/ecom frontend/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-// import  Login  from './components/Login'
-import './App.css'
-import { Login } from './components/login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import { Login } from './components/Login';
+import { SignUp } from './components/SignUp';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Login></Login>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
